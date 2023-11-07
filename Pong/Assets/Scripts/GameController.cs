6b452638c6fbc9581058b8c36ce5e0e2407d24e5
Maxomiliano <max.nebula1@gmail.com>
2023-11-06 22:39:06 -0300

    Getting core loop done
    After these changes, the ball instatiates correctly but just once

--- Pong/Assets/Scripts/GameController.cs
+++ Pong/Assets/Scripts/GameController.cs
@@ -10,8 +10,11 @@ public class GameController : MonoBehaviour
     [SerializeField] float _yPush = 1f;
     [SerializeField] float _speed = 1f;
     [SerializeField] public PlayerScore _playerScore;
+    //[SerializeField] Vector3 _lastPosition;
 
     private GameObject _cloneBall;
+    private Vector3 _ballInitialPosition;
+    private Quaternion _ballInitialRotation;
 
     private void OnEnable()
     {
@@ -19,6 +22,7 @@ public class GameController : MonoBehaviour
     }
     void Start()
     {
+        _ballInitialPosition = _ball.transform.position;
         //ResetGame();
         GoToRandomPosition();   //After a player make a point, it should call GoToRandomPosition() again
     }
@@ -29,12 +33,14 @@ public class GameController : MonoBehaviour
     }
     private void GoToRandomPosition()
     {
+        float xRandom = UnityEngine.Random.Range(-1f, 1f); //X nunca puede ser 0
+        float yRandom = UnityEngine.Random.Range(-1f, 1f); //Y nunca debería ser 0, salvo que la velocidad de Player modifique la reflexión de la pelota
         if (_ball != null)
         {
             //UnityEngine.Random.insideUnitCircle
-            float xRandom = UnityEngine.Random.Range(-1f, 1f); //X nunca puede ser 0
-            float yRandom = UnityEngine.Random.Range(-1f, 1f); //Y nunca debería ser 0, salvo que la velocidad de Player modifique la reflexión de la pelota
             _ball.GetComponent<Rigidbody2D>().velocity = new Vector2(xRandom, yRandom).normalized * _speed; //turn this into AddForce instead of Velocity
+            if (_cloneBall != null)
+                _cloneBall.GetComponent<Rigidbody2D>().velocity = new Vector2(xRandom, yRandom).normalized * _speed;
         }
     }
 
@@ -42,8 +48,9 @@ public class GameController : MonoBehaviour
     {
         if (_ball != null)
         {
-            _cloneBall = Instantiate(_ball);
-            _cloneBall.SetActive(true);
+            _cloneBall = Instantiate(_ball, _ballInitialPosition, _ballInitialRotation);
+            GoToRandomPosition();
+            //_cloneBall.SetActive(true);
             //Instantiate(_ball);
             //_ball.SetActive(true);
         }
@@ -51,7 +58,7 @@ public class GameController : MonoBehaviour
 
     void OnPointScored()
     {
-        GoToRandomPosition();
         ResetGame();
+        //GoToRandomPosition();
     }
 }
