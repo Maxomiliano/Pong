c591a2db9a26720cb6fd4f7976ba3c44ecd0cef9
Maxomiliano <max.nebula1@gmail.com>
2024-01-09 13:02:55 -0300

    Setting pvp

---
+++ Pong/Assets/Scripts/Player2Toggle.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Toggle : MonoBehaviour
+{
+    [SerializeField] GameObject _player2;
+
+    public void PlayerVersusPlayer()
+    {
+        _player2.GetComponent<PlayerMovement>().enabled = true;
+        _player2.GetComponent<TestAIMovement>().enabled = false;
+    }
+
+    public void PlayerVersusAI()
+    {
+        _player2.GetComponent<PlayerMovement>().enabled = false;
+        _player2.GetComponent<TestAIMovement>().enabled = true;
+    }
+}
